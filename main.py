25923a6a5be03fe76a070f08b6651a0bd7068b3e
Sophiya-ai <sophiyabr@gmail.com>
2024-10-25 15:35:59 +0200

    add class game

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import configuration as conf
+
 class Hero():
     def __init__(self, name):
         self.name = name
         self.health = conf.HEALTH
-        self.attack_power = ATTACK_POWER
+        self.attack_power = conf.ATTACK_POWER
+
+    def attack(self,other):
+        pass
+
+    def is_alive(self):
+        pass
+
+class Game():
+    def __init__(self, player:Hero(), computer:Hero()):
+        self.player = player
+        self.computer = computer
+
+    def start(self):
+        pass
