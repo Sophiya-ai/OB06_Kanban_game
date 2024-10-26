43002bdb02ec222d2821769579256954aca7b2a9
Sophiya-ai <sophiyabr@gmail.com>
2024-10-26 09:35:42 +0200

    developed start and tested start with the whole code

--- configuration.py
+++ configuration.py
@@ -1,6 +1,7 @@
 # константы игроков
 HEALTH = 100
 ATTACK_POWER = 20
+ATTACK_POWER_max = 100
 
 # константы игры
 
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Hero():
 
     def attack(self,other):
         if self.name == "computer":
-            self.attack_power = r.randint(conf.ATTACK_POWER,conf.ATTACK_POWER ** 5)
+            self.attack_power = r.randint(conf.ATTACK_POWER,conf.ATTACK_POWER_max)
         other.health -= self.attack_power
 
     def is_alive(self):
@@ -18,12 +18,23 @@ class Hero():
         return False
 
 class Game():
-    def __init__(self, player, computer):
+    def __init__(self,player,computer):
         self.player = player
         self.computer = computer
 
     def start(self):
-        pass
+        while hero1.is_alive() and hero2.is_alive():
+            print(f"Игрок {hero1.name.upper()} атакует!")
+            hero1.attack(hero2)
+            print(f"Оставшееся здоровье компьютера  - {hero2.health}")
+            print("Компьютер атакует!")
+            hero2.attack(hero1)
+            print(f"Оставшееся здоровье игрока {hero1.name.upper()}  - {hero1.health}")
+        if hero1.is_alive() == False:
+            print(f"Победа за {hero2.name.upper()}!!!")
+        elif hero2.is_alive() == False:
+            print(f"Победа за {hero1.name.upper()}!!!")
+
 
 def test_attack_is_alive():
     hero1 = Hero("Nina")
@@ -44,7 +55,13 @@ def test_attack_is_alive():
         print(f"hero2 - {hero2.health}")
         print(hero2.is_alive())
 
-    #тестирование метода attack
-test_attack_is_alive()
-# game = Game()
-# game.start()
+#тестирование метода attack
+#test_attack_is_alive()
+
+#текст игры
+print("Игра БИТВА ГЕРОЕВ началась!")
+hero1_name = input("Введите имя игрока: ")
+hero1 = Hero(hero1_name)
+hero2 = Hero("computer")
+game = Game(hero1, hero2)
+game.start()
